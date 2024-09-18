260d53eee79c0e56ca9d46da1860d3e2843b3486
Phuwit1 <65070184@kmitl.ac.th>
2024-09-18 14:18:33 +0700

    ลบsetting loginออก

--- kinkhaw_rueyang/apps/models.py
+++ kinkhaw_rueyang/apps/models.py
@@ -4,9 +4,7 @@ from django.contrib.auth.models import AbstractBaseUser
 class Customer(AbstractBaseUser):
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
-    
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []
+
 
 class Customer_Detail(models.Model):
     customer = models.OneToOneField("apps.Customer", on_delete=models.PROTECT)
