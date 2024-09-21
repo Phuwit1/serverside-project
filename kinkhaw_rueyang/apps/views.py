62688e0b4ff5400b01a867f2563040e62b09dbe4
Nongaomit <atikun145@gmail.com>
2024-09-21 21:25:34 +0700

    เพิ่ม categories

--- kinkhaw_rueyang/apps/views.py
+++ kinkhaw_rueyang/apps/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render,get_object_or_404
+from django.shortcuts import redirect, render
 from django.views import View
 from .models import *
 from .forms import *
@@ -96,54 +96,98 @@ class CartView(View):
 class ManageMenuView(View):
     def get(self, request):
         menu_items = Menu.objects.all()
+        categories = MenuCategory.objects.all()
+        
+        menu_with_categories = []
         for item in menu_items:
-            if not item.image:  
-                item.image = 'default_image.jpg'  # Notsure
-        return render(request, 'manage_menu.html', {'menu_items': menu_items})
-    
-
+            category_names = [category.name for category in item.menucategory_set.all()]
+            menu_with_categories.append({
+                'item': item,
+                'categories': category_names
+            })
+        
+        return render(request, 'manage_menu.html', {'menu_with_categories': menu_with_categories, 'categories': categories})
 
 class MenuCreateView(View):
     def get(self, request):
         form = MenuForm()
-        return render(request, 'menu_form.html', {'form': form})
+        categories = MenuCategory.objects.all()  
+        return render(request, 'menu_form.html', {'form': form, 'categories': categories})
 
     def post(self, request):
         form = MenuForm(request.POST, request.FILES)
         if form.is_valid():
-            try:
-                with transaction.atomic(): 
-                    form.save()
-                return redirect('manage_menu')
-            except Exception as e:
-                return render(request, 'menu_form.html', {'form': form, 'error': str(e)})
-        return render(request, 'menu_form.html', {'form': form})
+            menu_item = form.save()  
+            category_ids = form.cleaned_data.get('categories')           
+            if category_ids:
+                menu_item.menu_category.set(category_ids)  
+            return redirect('manage_menu')
+        categories = MenuCategory.objects.all()
+        return render(request, 'menu_form.html', {'form': form, 'categories': categories})
+class MenuCreateView(View):
+    def get(self, request):
+        form = MenuForm()
+        categories = MenuCategory.objects.all()
+        return render(request, 'menu_form.html', {'form': form, 'categories': categories})
+
+    def post(self, request):
+        form = MenuForm(request.POST, request.FILES)
+        if form.is_valid():
+            menu_item = form.save()  
+            category_ids = form.cleaned_data.get('categories') 
+            if category_ids:
+                menu_item.menucategory_set.set(category_ids)  
+            return redirect('manage_menu')
+        categories = MenuCategory.objects.all()  
+        return render(request, 'menu_form.html', {'form': form, 'categories': categories})
+
+
 
 class MenuEditView(View):
     def get(self, request, pk):
-        menu_item = get_object_or_404(Menu, pk=pk)
+        menu_item = Menu.objects.get(pk=pk)
         form = MenuForm(instance=menu_item)
-        return render(request, 'menu_form.html', {'form': form})
+        categories = MenuCategory.objects.all()
+        selected_categories = menu_item.menucategory_set.all()  
+        return render(request, 'menu_form.html', {
+            'form': form,
+            'categories': categories,
+            'selected_categories': selected_categories
+        })
 
     def post(self, request, pk):
-        menu_item = get_object_or_404(Menu, pk=pk)
+        menu_item = Menu.objects.get(pk=pk)
         form = MenuForm(request.POST, request.FILES, instance=menu_item)
         if form.is_valid():
-            try:
-                with transaction.atomic():  
-                    form.save()
-                return redirect('manage_menu')
-            except Exception as e:
-                return render(request, 'menu_form.html', {'form': form, 'error': str(e)})
-        return render(request, 'menu_form.html', {'form': form})
+            menu_item = form.save()
+            category_ids = form.cleaned_data.get('categories') 
+
+            if category_ids:
+               
+                menu_item.menucategory_set.clear()  
+                menu_item.menucategory_set.add(*category_ids)  
+        categories = MenuCategory.objects.all()  
+        return render(request, 'menu_form.html', {'form': form, 'categories': categories})
 
 class MenuDeleteView(View):
     def post(self, request, pk):
-        menu_item = get_object_or_404(Menu, pk=pk)
+        menu_item = Menu.objects.filter(pk=pk).first()
+        if menu_item is None:
+            return HttpResponse("<h1>ไม่พบเมนูนี้</h1>")
+        
         try:
-            with transaction.atomic():  
+            with transaction.atomic():
                 menu_item.delete()
             return redirect('manage_menu')
         except Exception as e:
             menu_items = Menu.objects.all()
             return render(request, 'manage_menu.html', {'menu_items': menu_items, 'error': str(e)})
+
+# class MenuCategoryView(View):
+#     def get(self, request, category_id):
+#         category = MenuCategory.objects.filter(id=category_id).first()
+#         if category is None:
+#             return HttpResponse("<h1>ไม่พบหมวดหมู่ของเมนูนี้</h1>")
+        
+#         menus = category.menu.all()
+#         return render(request, "menu_category.html", {'menus': menus, 'category': category})
