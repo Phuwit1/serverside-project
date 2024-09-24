1e06a0a901f64be4bd58b02de126c0b7316f776c
Phuwit1 <65070184@kmitl.ac.th>
2024-09-24 15:57:18 +0700

    'แก้นิดหน่อยลืมไปละแก้ไร'

--- kinkhaw_rueyang/authen/urls.py
+++ kinkhaw_rueyang/authen/urls.py
@@ -3,5 +3,6 @@ from authen.views import *
 from . import views
 urlpatterns = [
     path('', views.LoginView.as_view(), name='login'),
-    path('base/', views.BaseView.as_view(), name='base')
+    path('logout', views.LogoutView.as_view(), name='logout'),
+    path('base/', views.BaseView.as_view(), name='base'),
 ]
