689802b3ed4e61af700be1963b2834c1edce084f
Nongaomit <atikun145@gmail.com>
2024-09-28 05:27:40 +0700

    เก็บ role of user

--- kinkhaw_rueyang/authen/models.py
+++ kinkhaw_rueyang/authen/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+    ROLES = (
+        ('customer', 'Customer'),
+        ('shopkeeper', 'Shopkeeper'),
+        ('staff', 'Staff'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLES, default='customer')
+
+    def __str__(self):
+        return self.user.username
