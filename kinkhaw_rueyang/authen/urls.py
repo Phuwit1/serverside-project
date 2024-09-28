b7cdef772baccf464f38ea07abff11735ebf7df0
Nongaomit <atikun145@gmail.com>
2024-09-29 03:24:20 +0700

    ลบส่วนเกิน

--- kinkhaw_rueyang/authen/urls.py
+++ kinkhaw_rueyang/authen/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout'),
     path('base/', views.BaseView.as_view(), name='base'),
-    path('', RegisterView.as_view(), name='register')
 
 ]
