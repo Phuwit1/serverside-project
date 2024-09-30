b467872471d274fe5176a5073181e91f75404f3b
Nongaomit <atikun145@gmail.com>
2024-10-01 02:00:10 +0700

    ดึงร้านนั้นๆมา

--- kinkhaw_rueyang/authen/models.py
+++ kinkhaw_rueyang/authen/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-# from 
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
@@ -8,13 +7,14 @@ class UserProfile(models.Model):
         ("F", "Female"),
         ("O", "Other"),
     )
-    image = models.ImageField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=10)
-    email = models.EmailField()
-    sex = models.CharField(choices=SEX_CHOICE)
-    address = models.TextField()
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100, null=True, blank=True)
+    last_name = models.CharField(max_length=100, null=True, blank=True)
+    phone_number = models.CharField(max_length=10, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICE)
+    address = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='userimage/', null=True, blank=True)
+
     def __str__(self):
         return self.user.username
--- kinkhaw_rueyang/authen/urls.py
+++ kinkhaw_rueyang/authen/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import *
 from . import views
 urlpatterns = [
-    # path('')
     path('', RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout', views.LogoutView.as_view(), name='logout'),
     path('base/', views.BaseView.as_view(), name='base'),
     path('myprofile/', views.MyProfileView.as_view(), name="myprofile"),
+    path('shop_redirect/', ShopRedirectView.as_view(), name='shop_redirect'),
 ]
