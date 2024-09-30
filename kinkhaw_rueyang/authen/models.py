610dc038ccc8f04d764d34d8e66f54639c3aa81e
Nongaomit <atikun145@gmail.com>
2024-09-30 18:55:21 +0700

    Merge branch 'main' of https://github.com/Phuwit1/serverside-project

--- kinkhaw_rueyang/authen/models.py
+++ kinkhaw_rueyang/authen/models.py
@@ -1,19 +1,6 @@
 from django.db import models
 # from django.contrib.auth.models import User
 
-<<<<<<< HEAD
-# class UserProfile(models.Model):
-#     ROLES = (
-#         ('customer', 'Customer'),
-#         ('shopkeeper', 'Shopkeeper'),
-#         ('staff', 'Staff'),
-#     )
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)  # แก้ไข indent ที่นี่
-#     role = models.CharField(max_length=20, choices=ROLES, default='customer')
-
-#     def __str__(self):
-#         return self.user.username
-=======
 class UserProfile(models.Model):
     SEX_CHOICE = (
         ("M", "Male"),
@@ -28,4 +15,3 @@ class UserProfile(models.Model):
     sex = models.CharField(choices=SEX_CHOICE)
     def __str__(self):
         return self.user.username
->>>>>>> 50d2a1a0e90c6b19a5eda5a49a7a90c71712b01e
