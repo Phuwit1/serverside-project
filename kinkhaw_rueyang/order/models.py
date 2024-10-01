f0d4ecc9a18065dc96ddcefc7a79222316355c39
Phuwit1 <65070184@kmitl.ac.th>
2024-10-01 21:26:24 +0700

    test

--- kinkhaw_rueyang/order/models.py
+++ kinkhaw_rueyang/order/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+# Create your models here..
 class Order(models.Model):
     class Status(models.Choices):
         Order = "Order"
