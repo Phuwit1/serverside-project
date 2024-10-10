70d983f549d3159a8db3fa012d7d363e45fc812e
Nongaomit <atikun145@gmail.com>
2024-10-11 03:10:53 +0700

    done 100%จริงๆละ

--- kinkhaw_rueyang/authen/urls.py
+++ kinkhaw_rueyang/authen/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('base/', views.BaseView.as_view(), name='base'),
     path('myprofile/', views.MyProfileView.as_view(), name="myprofile"),
     path('myprofile/changepass/', views.ChangePasswordView.as_view(), name="changepass"),
-    path('shop_redirect/', ShopRedirectView.as_view(), name='shop_redirect'),
+    # path('shop_redirect/', ShopRedirectView.as_view(), name='shop_redirect'),
 ]
